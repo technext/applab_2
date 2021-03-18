ad77be2b86bc98251a3d55d0559aeae4905136e3
kanijfatemalaizu <kanij.fatema.laizu@gmail.com>
2021-03-18 12:36:59 +0600

    navbar

--- v1.0.0/assets/js/theme.js
+++ v1.0.0/assets/js/theme.js
@@ -295,6 +295,12 @@ var navbarInit = function navbarInit() {
     window.addEventListener(Events.SCROLL, function () {
       var scrollTop = html.scrollTop;
       var alpha = scrollTop / windowHeight * 0.15;
+      navbar.classList.add('backdrop');
+
+      if (alpha === 0) {
+        navbar.classList.remove('backdrop');
+      }
+
       alpha >= 1 && (alpha = 1);
       navbar.style.backgroundColor = "rgba(".concat(colorRgb[0], ", ").concat(colorRgb[1], ", ").concat(colorRgb[2], ", ").concat(alpha, ")");
       navbar.style.backgroundImage = alpha > 0 || utils.hasClass(navbarCollapse, 'show') ? backgroundImage : 'none';
